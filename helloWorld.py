3797b143d629b0aaf9a3721e15ecceb9456f8055
Malcoom <46626912+Malcoom@users.noreply.github.com>
2019-08-13 18:44:55 -0400

    Update helloWorld.py

--- helloWorld.py
+++ helloWorld.py
@@ -1,7 +1,11 @@
 #dimitri this is a program that runs inputs for quadratic functions, im testing it.
-cofX2 = input(float("Input coefficent for X^2 value: "))
-cofX1 = input(float("Input coefficent for X value:  "))
-integer = input(float("Input constant: "))
+cofX2 = input("Input coefficent for X^2 value: "))
+cofX1 = input("Input coefficent for X value:  "))
+integer = input("Input constant: "))
 xVal = input(float("Input value being run: "))
+xVal = float(xVal)
+cofX2 = float(cofX2)
+cofX1 = float(cofX1)
+integer = float(integer)
 yVal = cofX2*xVal*xVal+cofX1*xVal+integer
 print(yVal)
