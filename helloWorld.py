e1214a5a8a1650744a6b94cb987be2baf2223d83
Malcoom <46626912+Malcoom@users.noreply.github.com>
2019-08-13 13:22:36 -0400

    Update helloWorld.py

--- helloWorld.py
+++ helloWorld.py
@@ -1,4 +1,7 @@
-#test baby
-malcolm = input("What is malcolm? ")
-for i in malcolm:
-    print(i)
+#dimitri this is a program that runs inputs for quadratic functions, im testing it.
+cofX2 = input(float("Input coefficent for X^2 value: "))
+cofX1 = input(float("Input coefficent for X value:  "))
+integer = input(float("Input constant: "))
+xVal = input(float("Input value being run: "))
+yVal = cofX2*xVal*xVal+cofX1*xVal+integer
+print(yVal)
